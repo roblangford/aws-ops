848cc794e604f7fa74190b7b99a8e40c0a3e34b6
Rob Langford <rob.langford1@gmail.com>
2022-05-08 15:01:08 +1000

    fixed up EventBridge Rule name and description to be more generated based on input variables

--- uptime_downtime/terraform/aws_eventbridge_rule.tf
+++ uptime_downtime/terraform/aws_eventbridge_rule.tf
@@ -1,9 +1,9 @@
 resource "aws_cloudwatch_event_rule" "cloudwatch_event_rule_uptime_downtime" {
-  count = var.enable_example_eventbridge_rule_stop_ec2 ? 1 : 0 # Optional Component as an example
-  name                = "EC2-Downtime-UTC-${var.scheduled_stop_hour}:${var.scheduled_stop_minute}"
-  description         = "Turn off ${var.target_environment} environment EC2 Instances ${var.scheduled_stop_hour}:${var.scheduled_stop_minute} GMT"
+  count               = var.enable_example_eventbridge_rule_stop_ec2 ? 1 : 0 # Optional Component as an example
+  name                = "EC2-Downtime-${var.scheduled_stop_hour}h_${var.scheduled_stop_minute}min-GMT"
+  description         = "Turn off ${var.target_environment} environment EC2 Instances ${var.scheduled_stop_hour}h:${var.scheduled_stop_minute}min GMT"
   schedule_expression = local.scheduled_stop_cron_expression
-  
+
 }
 
 resource "aws_cloudwatch_event_target" "cloudwatch_event_target_uptime_downtime" {
